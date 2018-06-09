5607d4305f22d86d7814448927c42cfa462fa8e0
Joakim <21Joakim@users.noreply.github.com>
2018-06-09 12:29:55 +0200

    Spacing please

--- example/com/jockie/bot/example/command/arguments/CommandAddAll.java
+++ example/com/jockie/bot/example/command/arguments/CommandAddAll.java
@@ -15,7 +15,7 @@ public class CommandAddAll extends CommandImpl {
 		super.setDescription("Add all provided double numbers and get the result");
 		super.setAliases("addall", "aa");
 	}
-																																/* Primitive arrays are not supported yet */
+																	/* Primitive arrays are not supported yet */
 	public void onCommand(MessageReceivedEvent event, CommandEvent commandEvent, @Argument(description="decimal number") @Endless(minArguments=2, maxArguments=10) Double[] nums) {
 		double sum = 0;
 		for(int i = 0; i < nums.length; i++) {
